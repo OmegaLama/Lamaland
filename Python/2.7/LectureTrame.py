b0122fd0cc26fe028a6ab875bd7ced92f3c6a228
Mathieu Justeau <mathieu.justeau+git@gmail.com>
2016-03-30 10:14:30 +0200

    Add Vianney project

--- Python/2.7/LectureTrame.py
+++ Python/2.7/LectureTrame.py
@@ -36,6 +36,8 @@ for ts, pkt in dpkt.pcap.Reader(open(filename,'r')):
 
     if eth.type!=dpkt.ethernet.ETH_TYPE_IP:
         continue
+    elif eth.type!=dpkt.ethernet.ETH_TYPE_ARP:
+        continue
 
     ip=eth.data
     ipcounter+=1
@@ -92,32 +94,3 @@ print ("Nombre total de paquets ICMP :", icmpcounter)
 print ("Nombre total de paquets ARP :", arpcounter)
 print ("Nombre total de paquets DHCP :", dhcpcounter)
 print ("Nombre total de paquets HTTP :", httpcounter)
-
-# For each packet in the pcap process the contents
-for timestamp, buf in dpkt.pcap.Reader(open(filename,'r')):
-
-    # Print out the timestamp in UTC
-    print 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp))
-
-    # Unpack the Ethernet frame (mac src/dst, ethertype)
-    eth = dpkt.ethernet.Ethernet(buf)
-    print 'Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type
-
-    # Make sure the Ethernet frame contains an IP packet
-    # EtherType (IP, ARP, PPPoE, IP6... see http://en.wikipedia.org/wiki/EtherType)
-    if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-        print 'Non IP Packet type not supported %s\n' % eth.data.__class__.__name__
-        continue
-
-    # Now unpack the data within the Ethernet frame (the IP packet)
-    # Pulling out src, dst, length, fragment info, TTL, and Protocol
-    ip = eth.data
-
-    # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
-    do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
-    more_fragments = bool(ip.off & dpkt.ip.IP_MF)
-    fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
-
-    # Print out the info
-    print 'IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
-          (ip_to_str(ip.src), ip_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset)
