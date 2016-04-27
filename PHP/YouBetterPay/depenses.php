d2b13e2b7e1dd71daa3ddd9efde61508c6c36e05
Mathieu Justeau <mathieu.justeau+git@gmail.com>
2016-04-27 12:45:36 +0200

    Add some traitement.php

--- PHP/YouBetterPay/depenses.php
+++ PHP/YouBetterPay/depenses.php
@@ -15,26 +15,27 @@
 $connexion = mysqli_connect('localhost','lama','lama','db_test');
 $ok = mysqli_select_db($connexion,'diane') ;
 // Execution de la requete de selection.
-$requete_depenses = 'SELECT d.depense_id, d.depense_nom, d.depense_description, d.depense_prix, u.utilisateur_nom, u.utilisateur_prenom, u.utilisateur_pseudo 
-            FROM y_depenses d 
-            INNER JOIN y_utilisateurs u 
-            ON d.depense_payeur_id = u.utilisateur_id';
+$requete_depenses = 'SELECT d.depense_id \'Depense ID\', d.depense_nom \'Nom Depense\', d.depense_description \'Description\', d.depense_prix \'Prix\', d.depense_date \'Date Depense\', u.utilisateur_nom , u.utilisateur_prenom, u.utilisateur_pseudo 
+FROM y_depenses d 
+INNER JOIN y_utilisateurs u 
+ON d.depense_payeur_id = u.utilisateur_id 
+ORDER BY d.depense_date ASC ';
 $depenses = mysqli_query($connexion,$requete_depenses);
 // Lecture et affichage du resultat
 while ($depense = mysqli_fetch_assoc($depenses)) {
     ?>
     <ul>
-        <li>Depense: <?php echo $depense['depense_id']; ?></li>
+        <li>Depense: <?php echo $depense['Depense ID']; ?></li>
         <ul>
-            <li>Nom depense: <?php echo $depense['depense_nom']; ?></li>
-            <li>Prix depense: <?php echo $depense['depense_prix']; ?></li>
-            <li>Description: <?php echo $depense['depense_description']; ?></li>
-            <li>
-                Payeur: <?php echo $depense['utilisateur_prenom'], ' ', $depense['utilisateur_nom'], ', ', $depense['utilisateur_pseudo']; ?></li>
+            <li>Nom Depense: <?php echo $depense['Nom Depense']; ?></li>
+            <li>Prix Depense: <?php echo $depense['Prix'],' €'; ?></li>
+            <li>Description: <?php echo $depense['Description']; ?></li>
+            <li>Date Depense: <?php echo $depense['Date Depense']; ?></li>
+            <li>Payeur: <?php echo $depense['utilisateur_prenom'], ' ', $depense['utilisateur_nom'], ', ', $depense['utilisateur_pseudo']; ?></li>
             <li>Personne participant à l'achat:</li>
             <ul>
                 <?php
-                $preparation_requete_depenses_utilisateurs = 'SELECT u.utilisateur_prenom, u.utilisateur_nom, u.utilisateur_pseudo  
+                $preparation_requete_depenses_utilisateurs = 'SELECT u.utilisateur_prenom, u.utilisateur_nom, u.utilisateur_pseudo, jdu.jdu_part_utilisateur
                         FROM y_jointure_depenses_utilisateurs jdu 
                         INNER JOIN y_utilisateurs u 
                         ON jdu.jdu_id_utilisateur = u.utilisateur_id 
@@ -44,13 +45,23 @@ while ($depense = mysqli_fetch_assoc($depenses)) {
 
                 $stmt = mysqli_prepare($connexion, $preparation_requete_depenses_utilisateurs);
 
-                $ok = mysqli_stmt_bind_param($stmt, 'd', $depense['depense_id']);
+                $ok = mysqli_stmt_bind_param($stmt, 'd', $depense['Depense ID']);
                 $ok = mysqli_stmt_execute($stmt);
-                $ok = mysqli_stmt_bind_result($stmt, $utilisateur_prenom, $utilisateur_nom, $utilisateur_pseudo);
+                $ok = mysqli_stmt_bind_result($stmt, $utilisateur_prenom, $utilisateur_nom, $utilisateur_pseudo, $utilisateur_part);
 
                 while (mysqli_stmt_fetch($stmt)) {
                 ?>
-                    <li><?php echo $utilisateur_prenom, ' ', $utilisateur_nom, ', ', $utilisateur_pseudo; ?></li>
+                    <li><?php
+                        if ($utilisateur_pseudo) {
+                            echo $utilisateur_prenom, ' ', $utilisateur_nom, ', ', $utilisateur_pseudo;
+                        }
+                        else {
+                            echo $utilisateur_prenom, ' ', $utilisateur_nom;
+                        }
+                        ?></li>
+                    <ul>
+                        <?php echo 'Part de l\'utilisateur: ',$utilisateur_part,' €';?>
+                    </ul>
                 <?php } ?>
             </ul>
         </ul>
